f5c1098faa4e3b14d84d4178240db60de6a4dd4f
luooyii <luooyii@163.com>
2019-01-09 23:26:34 +0800

    初始化

---
+++ ferrry-server/src/main/kotlin/cn/luooyii/ferrry/server/FerrryServerApplication.kt
@@ -0,0 +1,12 @@
+package cn.luooyii.ferrry.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FerrryServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FerrryServerApplication>(*args)
+}
+
