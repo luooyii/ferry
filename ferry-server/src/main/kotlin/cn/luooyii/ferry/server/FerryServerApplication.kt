010f18940e86b75b8dff8849f05fdb667d8a1d2f
luooyii <luooyii@163.com>
2019-01-10 21:41:12 +0800

    init

---
+++ ferry-server/src/main/kotlin/cn/luooyii/ferry/server/FerryServerApplication.kt
@@ -0,0 +1,12 @@
+package cn.luooyii.ferry.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FerryServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FerryServerApplication>(*args)
+}
+
