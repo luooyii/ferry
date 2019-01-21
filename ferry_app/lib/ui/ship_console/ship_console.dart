1dc9d7932d51e89c21335bc0b95062f3d4d5690b
luooyii <luooyii@163.com>
2019-01-21 13:57:16 +0800

    navigation

---
+++ ferry_app/lib/ui/ship_console/ship_console.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShipConsole extends StatefulWidget {
+  @override
+  _ShipConsoleState createState() => _ShipConsoleState();
+}
+
+class _ShipConsoleState extends State<ShipConsole> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ ferry_app/lib/ui/user_center/user_center.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserCenter extends StatefulWidget {
+  @override
+  _UserCenterState createState() => _UserCenterState();
+}
+
+class _UserCenterState extends State<UserCenter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
