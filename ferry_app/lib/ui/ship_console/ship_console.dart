7444be0c93bfa3b19fdfde97f828b23800161744
luooyii <luooyii@163.com>
2019-01-28 14:09:02 +0800

    l

--- ferry_app/lib/ui/ship_console/ship_console.dart
+++ ferry_app/lib/ui/ship_console/ship_console.dart
@@ -13,28 +13,25 @@ class _ShipConsoleState extends State<ShipConsole> {
   @override
   Widget build(BuildContext context) {
     return new DefaultTabController(
-        length: 3,
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: Text("barSearch"),
-            bottom: new TabBar(
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.black12,
-              tabs: [
-                new Tab(text: "动力"),
-                new Tab(text: "方位"),
-                new Tab(text: "姿态"),
-              ],
-            ),
-          ),
-          body: new TabBarView(
-              children: [
-                ShipPower(),
-                ShipOrientation(),
-                ShipAttitude()
-              ]
+      length: 3,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: Text("barSearch"),
+          bottom: new TabBar(
+            indicatorColor: Color(AppColors.NavigationActive),
+            labelColor: Color(AppColors.NavigationActive),
+            unselectedLabelColor: Colors.black12,
+            tabs: [
+              new Tab(text: "动力"),
+              new Tab(text: "方位"),
+              new Tab(text: "姿态"),
+            ],
           ),
         ),
+        drawer: new Drawer(),
+        body: new TabBarView(
+            children: [ShipPower(), ShipOrientation(), ShipAttitude()]),
+      ),
     );
   }
 }
